96857ea53ccbf011932883d10c92077fab6d1e85
MikeWang <hi.from.mikewang@gmail.com>
2015-05-26 23:15:55 +0800

    人脸识别

---
+++ src/com/crazylin/facedetected/Constant.java
@@ -0,0 +1,6 @@
+package com.crazylin.facedetected;
+
+public class Constant {
+	public static final String KEY = "a9f32d6fd94da04bc162caabe7e87400";
+	public static final String SECRET = "BP9B_33cGnuwPOi4cq2bqNTTTeeLb3cV";
+}
